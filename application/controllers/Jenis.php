babb0e977bf5566d51207b6cd97a2e0cdb938799
Taufik Hidayat <taufik1799hidayat@gmail.com>
2019-10-11 08:19:00 +0700

    mulai

---
+++ application/controllers/Jenis.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Jenis extends CI_Controller
+{
+    
+  public function __construct()
+  {
+    
+  }
+
+  public function index()
+  {
+    
+  }
+
+}
